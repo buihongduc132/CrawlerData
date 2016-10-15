b329eeb5c8653c25fc3beb4cb8dfebe24dd330e3
Duc Bui <buihongduc132@yahoo.com>
2016-10-15 16:27:58 +0700

    add tests for fileHelper

--- Helper/fileHelper.js
+++ Helper/fileHelper.js
@@ -3,16 +3,31 @@ var fs = require('fs');
 var path = require('path');
 Promise.promisifyAll(fs);
 var config = require('../config.json');
+var _ = require('lodash');
 
 var readFile = function(inputPath) {
     return fs.readFileAsync(inputPath, config.fileEncoding);
 }
 
-var writeFile = function(inputPath) {
-    throw new Error('Not yet implemented (readFile)');
+var writeFile = function(inputPath, data) {
+    return fs.writeFileAsync(inputPath, data, config.fileEncoding);
+}
+
+var getFiles = function(inputPath) {
+    return fs.readdirAsync(inputPath);
+}
+
+var getFilesByType = function(inputPath) {
+    return Promise.resolve(fs.readdirAsync(inputPath).then((fileNames) => {
+        return _.filter(fileNames, (fileName) => {
+            return fileName.substr(fileName.length-5, fileName.length) === '.json';
+        });
+    }));
 }
 
 module.exports = { 
     readFile: readFile,
-    writeFile: writeFile
+    writeFile: writeFile,
+    getFiles: getFiles,
+    getFilesByType: getFilesByType
 }
--- Test/fileHelper.spec.js
+++ Test/fileHelper.spec.js
@@ -10,12 +10,42 @@ describe('Data Helper', () => {
     describe('Read file', () => {
         it('should find the specific file', () => {
             fileHelper.readFile(config.path.test.readFile).then((result) => {
-                expect(result).to.not.be.null;
+                expect(result).not.equal(null);
             });
         });
         it('should return the correct text', () => {
             fileHelper.readFile(config.path.test.readFile).then((result) => {
-                expect(result).is.equal('This is test for reading file');
+                expect(result).equal('This is test for reading file');
+            });
+        })
+    });
+
+    describe('Write file', () => {
+        it('should create and write file with correct content', () => {
+            var seed = JSON.stringify(Math.random());
+            fileHelper.writeFile(config.path.test.writeFile, seed).then(() => {
+                fileHelper.readFile(config.path.test.writeFile).then((result) => {
+                    expect(result).equal(seed);
+                })
+            })
+        })
+    });
+
+    describe('Read directory', () => {
+        it('should get all files name in test folder', () => {
+            var fileNames = ["File1", "File2.json", "File3.xml", "File4.json"];
+            fileHelper.getFiles(config.path.test.readDir).then((result) => {
+                expect(result).deep.equal(fileNames);
+            });
+        });
+    });
+
+    describe('Read directory for find type', () => {
+        it('it should get all the json file in test folder', () => {
+            var fileNames = ["File2.json", "File4.json"];
+
+            fileHelper.getFilesByType(config.path.test.readDir).then((result) => {
+                expect(result).deep.equal(fileNames);
             });
         })
     });
